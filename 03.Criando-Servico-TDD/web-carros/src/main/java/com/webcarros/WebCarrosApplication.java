bc9ba42d768b73371b2f7d65aca8b5d7f819eaa4
Tiago Lima <lima.tss@gmail.com>
2020-10-12 16:58:40 -0300

    Adicionando a terceira etapa

---
+++ 03.Criando-Servico-TDD/web-carros/src/main/java/com/webcarros/WebCarrosApplication.java
@@ -0,0 +1,29 @@
+package com.webcarros;
+
+import java.time.LocalDate;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.webcarros.domain.entities.Person;
+import com.webcarros.domain.repositories.PersonRepository;
+
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootApplication
+@Slf4j
+public class WebCarrosApplication {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(WebCarrosApplication.class, args);
+	}
+	
+	@Bean
+	CommandLineRunner initDatabase(final PersonRepository repository){
+		return args -> {
+			log.info("Saving... " + repository.save(new Person("01234567887", "Ubuntu", "ubuntu@ubuntu.com.br", LocalDate.parse("2000-01-01"))));
+		};		
+	}
+}
